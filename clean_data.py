9ec68b3ea57332ca4db3b60352ce0f18d17ad636
Klaudia Szpyrka <Klaudyna@Klaudias-MBP.lan>
2019-09-01 18:04:30 +0200

    Fix source data (remove invalid characters)

---
+++ clean_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+castles = pd.read_csv(r"castles.csv")
+cities = pd.read_csv(r"cities.csv")
+flixbus = pd.read_csv(r"flixbus.csv")
+gardens = pd.read_csv(r"gardens.csv")
+museums = pd.read_csv(r"museums.csv")
+zoos = pd.read_csv(r"zoos.csv")
